253271631da897293b847ee6e548f8e7542f732d
alyzenjeraj <alyzenjeraj@gmail.com>
2024-09-05 09:02:19 -0400

    feat: initial setup with aliases and docker container

---
+++ README.md
@@ -0,0 +1,24 @@
+# MTE 380 - Stewart Platform Project
+## Running the Docker Container
+1. Install [Docker Engine](https://docs.docker.com/engine/install/)
+2. Clone this repository
+3. `cd` into the root of this repo
+4. Run `source ws-aliases.sh`
+5. Run `ws-build` to build the image
+6. Run `ws-start` to start the image
+7. Run `ws-exec` to open an interactive shell into the container
+
+
+To stop using the shell, run `exit` while inside the container, and `ws-stop` to completely stop and remove the container
+
+Example:
+```bash
+mkdir -p ~/git/stewart-platform
+cd ~/git/stewart-platform
+git clone git@github.com:alyzenjeraj/stewart-platform.git . # Will need to set up SSH keys
+source ws-aliases.sh
+ws-build
+ws-start
+ws-exec
+
+```
---
+++ ws-aliases.sh
@@ -0,0 +1,33 @@
+# ROS2 Container ID
+ROS2_CONTAINER_ID=""
+
+# Alias to build the Docker image
+alias ws-build="docker build -t stewart_platform_ros2 -f docker/Dockerfile ."
+
+# Function to run the Docker container interactively with a mounted volume
+function ws-start {
+    local workspace_dir=$(pwd)
+    ROS2_CONTAINER_ID=$(docker run -d -it -v $workspace_dir/ros2_ws:/ros2_ws stewart_platform_ros2)
+    echo "Started container with ID: $ROS2_CONTAINER_ID"
+}
+
+# Function to attach to the running container using
+function ws-exec {    
+    if [ -n "$ROS2_CONTAINER_ID" ]; then
+        docker exec -it "$ROS2_CONTAINER_ID" bash
+    else
+        echo "No container is currently running. Start a container first."
+    fi    
+}
+
+# Function to stop and remove the running container
+function ws-stop {
+    if [ -n "$ROS2_CONTAINER_ID" ]; then
+        docker stop "$ROS2_CONTAINER_ID" && docker rm "$ROS2_CONTAINER_ID"
+        echo "Stopped and removed container with ID: $ROS2_CONTAINER_ID"
+        ROS2_CONTAINER_ID=""
+    else
+        echo "No container is currently running."
+    fi
+}
+
